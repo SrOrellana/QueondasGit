503eeb428c4fb4236442abe4de9228db94ecbd91
SrOrellana <54814932+SrOrellana@users.noreply.github.com>
2019-09-02 23:59:18 -0600

    Ejercicio 18
    No fue concluido.

---
+++ Ejercicio18.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+const int fil=5
+const int col=5
+ 
+int main() {
+    int matrix[fil][col];
+    for(int k=0;k<fil;k++){
+        for(int j=0;j<col<j++){
+            if(k>j){
+                matrix[f][c]=0;
+                {else{
+                    matrix[k][c]=1+
+                }
+            }
+        }
+    }
+    return 0;
+}
+
